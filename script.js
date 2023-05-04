04344ad8de3d3b306854fdb6791ec003721353e3
solarcalcados <82035186+solarcalcados@users.noreply.github.com>
2023-05-04 11:56:26 -0300

    uau

--- script.js
+++ script.js
@@ -1,4 +1,275 @@
 function mobMenuPlay(){
     document.querySelector(".nav-list-mobile").classList.toggle("active")
 }
-console.log("7")
+console.log("8")
+
+
+
+const Text = {
+    ex1: {
+        title: "Preparo e orientação colono.",
+        text: `
+                <pre>VÉSPERA DO EXAME: (A partir do jantar)
+    • Dieta sem fibras ou resíduos: (Pode ser tudo líquidos), galinha ou peixe bem cozidos, moídos
+    ou desfiados (caldos, ovos quentes ou gemada, biscoitos, legumes bem cozidos em sopas ou
+    purês, geleias.).
+        Obs: Não comer nada cru, com casca, com semente ou fibra.
+                
+    • Ingerir bastante líquido: Tomar água de coco, chá mate, suco de cor clara, um copo grande
+    (300ml), de hora em hora.
+    • Não tomar nada com a coloração vermelho ou laranja
+    • As 18:00h tomar 06 comprimidos de Ducolax ou Lacto Purga.
+                
+DIA DO EXAME
+    As 06:00 h da manhã, misturar em uma Jarra Grande:
+    - O MANITOL a 20% - 01 Frasco de 500 ml (feito em farmácias de manipulação)
+    - A mesma quantidade de água, suco ou chá (sem açúcar)
+    - 1 Frasco de SIMETICONA 10ml
+                
+    • COMEÇAR a tomar às 06:00h da manhã, um copo comum, de 15 em 15 minutos,
+    terminar a solução no máximo às 07:30h.
+    • Depois de tomado, ir ao banheiro quantas vezes for necessário.
+    • NÃO ALMOÇAR
+    Pode ingerir água se sentir sede até as 12:00 hs no dia do exame
+    Obs: A limpeza intestinal é indispensável para a realização do exame.
+    Após ingerir a solução do manitol, permanecer em jejum até a hora do exame, podendo tomar
+    água até às 12:00hs.
+
+    EXAME POR ORDEM DE CHEGADA
+    Trazer laudos de colonoscopias anteriores, se houver.
+    • OBS: VIM ACOMPANHADO PARA O EXAME COM UMA PESSOA MAIOR DE
+    IDADE.
+    • NECESSARIO QUE O ACAMPANHANTE SAIBA DIRIGIR.
+    • AS MULHERES DE VESTIDO OU SAIA.
+    • OS HOMENS DE BERMUDA DE ELÁSTICO.
+    Dica: TOMAR SORO CASEIRO A VONTADE (De 01 a 02 LITROS APÓS TERMINAR DE TOMAR
+    TODA A SOLUÇÃO).
+    MODO DE PREPARO:
+    01 LITRO DE ÀGUA FILTRADA, FERVIDA OU MINERAL ENGARRAFADA.
+    01 COLHER DE SOPA BEM CHEIA DE AÇÚCAR OU 02 COLHERES RASAS (20g).
+    01 COLHER DE CAFÉ DE SAL (3.5g).
+    ORIENTAÇÃO AO PACIENTE
+    COLONOSCOPIA
+
+    1. O que é?
+    É um procedimento que permite a visão do cólon (intestino grosso) e reto através de um
+    aparelho que possui uma microcâmera e é introduzido no ânus. O procedimento é muito
+    solicitado na investigação da maioria dos pacientes com sintomas intestinais para
+    diagnosticar doenças, remover pólipos (pequenos tumores malignos ou benignos) e obter
+    biópsias (retirada de amostras de tecidos para análise).
+
+    2. Como é feita?
+    O preparo para o exame é realizado com uma dieta leve líquida na véspera, e a ingestão
+    de laxativos horas antes do exame, com objetivo de eliminar todo o conteúdo de fezes do
+    cólon possibilitando a visualização da mucosa do intestino grosso. O preparo adequado
+    do cólon é essencial para a realização do exame, para isso siga as orientações
+    cuidadosamente.
+    O exame é realizado com paciente em posição lateral e as pernas dobradas. Durante o
+    procedimento são administrados sedativos e analgésicos para garantir o conforto do
+    paciente. Devido a administração dos sedativos é obrigatório a presença de acompanhante
+    para a realização do exame. Quando há necessidade de biópsia uma pinça é passada
+    através do colonoscópio, a fim de retirar alguns fragmentos do tecido e enviá-los para
+    análise de um patologista. Lembre-se que biópsias podem ser realizadas por várias razões,
+    como para inflamações, retiradas de pólipos, não significando, necessariamente suspeita
+    de câncer. Nem todos os pólipos, em especial os muito pequenos, são detectáveis. A
+    ressecção de pólipos não necessariamente garante a ausência do câncer ou câncer curável
+    em seguimento de 3 anos. Esse procedimento é realizado no Setor de Endoscopia, com o
+    paciente ficando em observação no próprio setor (recebendo alta após o mesmo).
+
+    3.Quais são os principais riscos?
+    São raras as complicações, mas pode haver:
+    ✓ Desconforto abdominal após o exame;
+    ✓ Dor no local da injeção de sedativos acompanhada por vômito
+    ✓ Náuseas e vômitos
+    ✓ As complicações relacionadas à sedação intravenosa são raras e de solução imediata
+    ✓ Raramente podem ocorrer perfurações e hemorragias - casos extremos (e muito raros)
+    - requerendo internação e eventual tratamento cirúrgico se necessário.
+    Sinais de alerta a serem observados:
+    ✓ Sangramento intenso nas fezes ✓ Temperatura maior que 38 ºC
+    ✓ Dor abdominal intensa ✓ Calafrios
+    Os sangramentos em grande volume são raros e podem manifestar-se com cólica, diarréia
+    com sangue, sinais de baixa pressão arterial (como desmaios, escurecimento da visão,
+    tonturas ao levantar). Os sangramentos tardios também podem ocorrer em até 15 dias
+    após a realização de polipectomia apresentando-se nas fezes, que na maioria das vezes é
+    autolimitado.
+
+    4. Cuidados após o procedimento nas primeiras 12h:
+    a. Dieta leve.
+    b. A ingestão de bebidas alcoólicas deverá ser evitada.
+    c. Restrição à atividade física.
+    d. Não se pode dirigir ou trabalhar no dia do exame. Se necessário solicitar atestado
+    médico.
+    e. Não é comum a ocorrência de dor significativa, que necessite de tratamento. Caso
+    ocorra dor podem ser usados analgésicos comuns, exceto aqueles que contenham ácido
+    acetilsalicílico / aspirina.
+    f. O uso de anticoagulantes deverá ser analisado caso a caso, entre o médico endoscopista
+    e o médico do paciente.
+
+    5. Como retirar o resultado?
+    O resultado do exame poderá ser pego no setor de entrega de exames da unidade na qual
+    foi realizado.
+    O resultado do exame para ser corretamente interpretado deve ser relacionado com uma
+    série de outros dados que o seu médico possui. Desse modo, qualquer dúvida em relação
+    ao resultado do exame de colonoscopia deve ser esclarecida com o médico que o solicitou.
+
+    6. OBSERVAÇÕES:
+    Não devem ser usados medicamentos contendo ácido acetil-salicílico (AAS, Aspirina,
+    Melhoral, Fontol, Bufferin, Somalgin, Cibalena, Sonrisal, Engov, Doril, etc) ou outros
+    que diminuem a coagulação sanguínea, como clopidogrel, anticoagulantes (marcoumar,
+    Xarelto, etc) nos 10 (dez) dias que antecedem o exame, salvo se a necessidade do exame
+    for urgente. Nesse caso, sempre informe o enfermeiro ou o médico que irá atendê-lo para
+    que sejam tomados os cuidados necessários. Esses medicamentos podem atrapalhar a
+    coagulação do sangue e, caso haja necessidade de alguma intervenção cirúrgica pela
+    endoscopia, deverá ser previsto o risco de fazê-la ou adiá-la para quando for mais seguro.
+    Se possível, não devem ser usados medicamentos orais para tratamento de anemia nos 7
+    (sete) dias que antecedem o exame. Eles coram as fezes de negro, atrapalhando a
+    qualidade do preparo do intestino e dificultando a observação da mucosa. Em casos de
+    urgência do exame, favor alertar o(s) médico(s) e enfermeiros que irão atendê-lo(a) no
+    exame.
+    Também não devem ser usadas medicações para diminuir o número de evacuações, como
+    Imosec, Lomotil, etc. pois podem atrapalhar a qualidade do preparo do intestino.</pre>
+                
+        `
+    },
+    ex2: {
+        title: "titulo1",
+        text: `
+        <pre>
+RECOMENTAÇÕES PARA O PACIENTE:
+    • Comparecer ao serviço de endoscopia, com antecedência mínima de 15 minutos.
+    • Estar acompanhado de pessoa maior de 18 anos em perfeitas condições de saúde
+    física e mental. Não trazer consigo crianças.
+    • Paciente hipertenso é necessário fazer controle diário de PA pelo menos 3 dias antes
+    do exame, apresentar o controle antes do exame.
+        NOTA: Pacientes diabéticos, insulinodependentes deverão realizar exames pela manhã,
+        prioritariamente nos primeiros horários.
+        É importante que o paciente traga os nomes dos medicamentos em uso, bem como laudos de
+        exames endoscópicos realizados anteriormente.
+
+EXAMES AGENDADOS PARA O PERÍODO MATINAL
+    • Jejum absoluto de 12 horas, inclusive água. Medicamentos de uso contínuo (antihipertensivos etc.) devem ser ingeridos com pouca água (01 xícara pequena) de
+    preferência até 2 horas antes do horário previsto para o exame.
+
+CUIDADOS POS EXAMES
+    • Não dirigir veículos de qualquer espécie e também não realizar trabalhos em altura
+    (utilização de escadas, andaimes ou outros objetos), por um período mínimo de 12
+    horas após exame.
+    • Após o exame não utilizar-se de transporte por motociclista ou bicicleta nem como
+    condutor ou carona.
+    • Ao completar o período de 01 hora após o exame pode iniciar a dieta por via oral, com
+    cautela para evitar possível aspiração. Recomenda-se iniciar com dieta pastosa.
+        </pre>
+        `
+    },
+    ex3: {
+        title: "titulo1",
+        text: `
+            <pre>
+Seguindo as recomendações da Sociedade Brasileira de Cardiologia e em respeito a você gostaríamos de prestar alguns
+esclarecimentos a respeito do Teste Ergométrico.
+
+O Teste Ergométrico é um exame usado para rastreamento de doença coronariana, identificação de arritmias,
+avaliação da resposta da pressão arterial, bem como da capacidade física, além de ser usado na avaliação pré-operatória
+em conjunto com os demais exames complementares.
+
+Para a realização do exame pode ser necessário fazer raspagem de pelos de alguns pontos do tórax (no caso dos
+homens) para colocação de eletrodos e limpeza da pele com gaze e álcool para retirada da oleosidade natural da região.
+Geralmente esse procedimento é bem tolerado, mas alguns pacientes podem apresentar desconforto como vermelhidão
+local por hipersensibilidade ou reação alérgica.
+
+O exame consiste em um teste de esforço em esteira rolante, onde há dificuldade progressiva pelo aumento da
+velocidade e inclinação do aparelho até o limite tolerado pelo paciente. Serão feitos registros eletrocardiográficos antes,
+durante e após o esforço, além de medidas seriadas da pressão arterial e monitorização cardíaca contínua.
+O exame poderá ser interrompido a qualquer momento por solicitação do paciente devido cansaço ou outro
+desconforto ou pelo médico responsável, se assim julgar necessário.
+
+Existe a possibilidade de aparecer sintomas como tontura, falta de ar, dor no peito, desmaio, alterações na
+pressão arterial e no ritmo cardíaco, sendo mínimas as chances de ocorrer eventos fatais (01 óbito para cada 10.000
+exames). Caso ocorram eventos/complicações durante o exame dispomos de procedimentos de emergência e pessoal
+treinado para lidar com tais situações.
+
+Como os sintomas ou reações adversas são mais frequentes em determinados grupos de pacientes, solicitamos
+que responda o questionário a seguir para avaliarmos o seu grau de risco.
+Declaro que fui devidamente informado (a) e autorizo a realização do exame de Teste Ergométrico e também o
+corpo clínico a praticar todos os atos médicos e de enfermagem dele decorrentes. Também entendi que, a qualquer
+momento poderei revogar este consentimento, antes da realização do procedimento.</pre>
+        `
+    },
+    ex4: {
+        title: "titulo1",
+        text: `
+            <pre>
+No dia anterior do exame tomar:
+
+    40 gotas de luftal depois do Almoço
+    40 gotas de luftal depois do Jantar.
+    40 gotas de luftal antes do Exame.
+
+    *Almoço e jantar leve, de preferência sopa, frango ou peixe.
+    *Não é recomendável leite e seus derivados, comidas pesadas,
+    bebidas alcoólicas e refrigerantes.
+    *Não ingerir nada depois das 20:00 hs.
+    *Vir em jejum total no dia do exame.
+    *Tomar 2 copos com água antes de sair de casa.</pre>
+        `
+    },
+    ex5: {
+        title: "titulo1",
+        text: `
+            <pre>
+EXAME REALIZADO A TARDE
+    VESPERA DO EXAME
+        • Tomar 2 comprimidos de Lacto-purga ou Dulcolax as 18:00 horas
+        • Jantar Leve
+        • Não ingerir: leites e derivados, gorduras, verduras, carnes vermelhas,
+        refrigerante, massas, café, bebidas alcoólicas.
+        • Tomar 20 gotas de Simeticona (luftal) as 22:00h.
+    DIA DO EXAME
+        • Tomar 20 gotas de Simeticona (luftal) as 06:00 da manhã.
+        • Tomar 20 gotas de Simeticona (luftal) 01 hora antes do exame.
+            ATENÇÃO: O paciente poderá até as 09:00h da manhã, realizar um lanche
+            (chá com torradas ou biscoito de “agua e sal” (sem manteiga) e após esta
+            refeição manter-se em jejum até a realização do exame.
+            Obs: Paciente utilizando medicação controloda, deverá manter o uso do medicamento
+            com mínimo de água possível.
+            
+PREPARO PARA REALIZAR PELA MANHÃ
+    VESPERA DO EXAME
+        • Tomar 2 comprimidos de Dulcolax as 18:00h
+        • Jantar Leve - Não ingerir: leites e derivados, gorduras, carnes vermelhas,
+        refrigerante, massas, café, bebidas alcoólicas.
+        • Tomar 20 gotas de Simeticona (luftal) as 22:00h após esse horário Jejum
+        Absoluto.
+    DIA DO EXAME
+        • Tomar 20 gotas de Simeticona (luftal) as 06:00 da manhã.
+        • Tomar 20 gotas de Simeticona (luftal) 01 hora antes do
+        exame.
+        Obs: Paciente utilizando medicação controlado, deverá manter o uso do medicamento
+        com mínimo de água possível.
+        </pre>
+        `
+    },
+
+}
+
+
+
+document.querySelector(".js-preparo-title").innerHTML= Text.ex1.title
+document.querySelector(".js-preparo-text").innerHTML= Text.ex1.text
+
+if(document.querySelector(".preparacao")){
+    document.querySelectorAll(".preparacao li").forEach((e)=>{
+        console.log(e.getAttribute("id"))
+        e.addEventListener("click", (e)=>{
+            document.querySelector(".js-preparo-title").innerHTML= e.target.innerHTML
+            document.querySelector(".js-preparo-text").innerHTML= Text[e.target.getAttribute("id")].text
+            document.querySelectorAll(".preparacao li").forEach((e)=>{
+                    e.classList.remove("active")
+                }
+            )
+            e.target.classList.toggle("active")
+        })
+    })
+        
+    
+}
